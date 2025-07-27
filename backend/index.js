3086fcb117ec2445d6cb96309c0ae01f0404f311
qqSatyam <satyam.jha@quantumquest.in>
2025-07-28 01:20:59 +0530

    added backend for e-commerce

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import {notFound, errorHandler} from "./middleware/errorHandler.js";
+import productRoute from './routes/product.route.js'
 
 
 dotenv.config();
+connectDb();
 
 const port = process.env.PORT || 5000
 const app = express();
@@ -10,6 +14,10 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('API is running')
 })
+
+app.use('/api/product', productRoute);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`)
