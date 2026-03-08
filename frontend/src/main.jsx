8117c17694ebfe994de99d5606def7f8152b56ce
satyamJha002 <jh.satyam@gmail.com>
2026-03-08 19:12:34 +0530

    product card updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import {Provider} from "react-redux";
-import store from './store.js'
+import { Provider } from "react-redux";
+import store from "./store.js";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "964714544510-80v1d58hnlomni82v2383ea1hkeodahi.apps.googleusercontent.com";
+const googleClientId =
+  import.meta.env.VITE_GOOGLE_CLIENT_ID ||
+  "964714544510-pu29n9v8qu8ngbkh5bspb4ujdh6tqlol.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -18,5 +20,5 @@ createRoot(document.getElementById("root")).render(
         </BrowserRouter>
       </GoogleOAuthProvider>
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
