a1542559a934ed7f64cf8852d1deae4324fde3fe
satyamJha002 <jh.satyam@gmail.com>
2026-08-24 22:58:12 +0530

    duplication error is resolved

--- backend/models/auth.model.js
+++ backend/models/auth.model.js
@@ -6,20 +6,26 @@ const authSchema = new mongoose.Schema(
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String },
-    googleId: { type: String, unique: true },
+    googleId: { type: String },
     role: { type: String, enum: ["user", "admin"] },
     isAdmin: { type: Boolean, default: false, required: true },
     refreshToken: { type: String, default: null },
   },
-  { timestamps: true }
+  { timestamps: true },
+);
+
+authSchema.index(
+  { googleId: 1 },
+  { unique: true, partialFilterExpression: { googleId: { $type: "string" } } },
 );
 
 authSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
-    next();
+    return next();
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 authSchema.methods.matchPassword = async function (enteredPassword) {
